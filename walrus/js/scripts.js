9045e73710e8a48a7c9688bf79fb986af4fd7f2b
Michael Mayer <mjmayer@ucdavis.edu>
2019-07-05 16:18:40 -0700

    dom manipulation

--- walrus/js/scripts.js
+++ walrus/js/scripts.js
@@ -2,4 +2,43 @@ $(document).ready(function() {
     $(".clickable").click(function() {
         $("#walrus").slideToggle();
     })
+
+    $("#foo").click(function() {
+       $("#clicked").prepend("<li>Foo</li>");
+       $("#response").prepend("<li>What?</li>");
+        $("ul#clicked").children("li").first().click(function(){
+            $(this).remove();
+        })
+        $("ul#response").children("li").first().click(function(){
+            $(this).remove();
+        })
+    })
+
+    $("#bar").click(function() {
+        $("#clicked").prepend("<li>Bar</li>");
+        $("#response").prepend("<li>What?</li>");
+        $("ul#clicked").children("li").first().click(function(){
+            $(this).remove();
+        })
+        $("ul#response").children("li").first().click(function(){
+            $(this).remove();
+        })
+    })
+
+    $("#baz").click(function() {
+        $("#clicked").prepend("<li>Baz</li>");
+        $("#response").prepend("<li>What?</li>");
+        $("ul#clicked").children("li").first().click(function(){
+            $(this).remove();
+        })
+        $("ul#response").children("li").first().click(function(){
+            $(this).remove();
+        })
+    })
+
+
+    $("#night-mode").click(function() {
+        $("body").toggleClass("night-mode")
+    })
+
 });
